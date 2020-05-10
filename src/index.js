7de4b7984f6c1b00d9e61cba8d4a020e8a4ce10b
hanafi <hanafi@nusaapartindo.com>
2020-05-10 14:57:58 +0700

    crate store at root component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {Provider} from "react-redux";
+import { createStore } from "redux";
+import reducers from "./redux/reducers";
 
 // BOOTSTRAP CSS
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById("root")
+);
